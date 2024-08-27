2ae1770bcb6c740209bb26659c518c25bc658e86
Isc Miguel Gonzalez <isc.mangel.glzdelarosa@gmail.com>
2024-08-27 12:37:42 -0600

    Se agregan variables de entorno

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,7 @@
 const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=PFo2ItCqoVcTF5GU97T4oinMbY2k7w2n&q=${ category }&limit=21`
+    const apiKey = import.meta.env.VITE_GIPHY_API_KEY
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${ category }&limit=21`
     const response = await fetch(url)
     const { data } = await response.json()
 
